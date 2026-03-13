4bb141cf25b302e312622402d344b43c7c9b5e54
alexander <malyugin.alexander98@gmail.com>
2026-03-13 18:17:26 +0100

    First commit

--- modules/gpr.py
+++ modules/gpr.py
@@ -3,7 +3,11 @@ import gpytorch
 
 class ExactGPModel(gpytorch.models.ExactGP):
 
-    def __init__(self, train_x, train_y):
+    def __init__(self,
+                 train_x,
+                 train_y,
+                 initial_kernel_parameters
+                 ):
         super(ExactGPModel, self).__init__(train_x, train_y, gpytorch.likelihoods.GaussianLikelihood())
 
         dim = train_x.shape[1]
@@ -11,8 +15,8 @@ class ExactGPModel(gpytorch.models.ExactGP):
         self.mean_module = gpytorch.means.LinearMean(input_size = dim)
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims = dim))
 
-        self.covar_module.base_kernel.lengthscale = torch.rand(dim) * 500 + 800
-        self.covar_module.outputscale = 1.0
+        self.covar_module.base_kernel.lengthscale = initial_kernel_parameters['lengthscale']
+        self.covar_module.outputscale = initial_kernel_parameters['outputscale']
 
     def forward(self, x):
         mean_x = self.mean_module(x)
